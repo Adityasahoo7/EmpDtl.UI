7722e43a62cb14dadd15548f239164d2dd3c2a0b
Aditya Sahoo <sahooaditya2003@gmail.com>
2026-05-13 11:23:35 +0530

    Add typescript v1

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
   styleUrls: ['./employee.component.css']
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit{
+
+  employees:Employee[] =[];
+
+  employee:Employee={
+    name:'',
+    phone:'',
+    email:'',
+    department:'',
+    designation:'',
+    salary:0,
+    managerid:0
+  };
+
+  isEdit = false;
+  /**
+   *
+   */
+  constructor() {
+    
+  }
+
 
 }
